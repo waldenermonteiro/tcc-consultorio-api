dbba304394183894c6c0ddb3d6eaea0d35866dde
Waldener Monteiro <jjrr019@gmail.com>
2020-05-03 20:19:57 -0300

    feature: criando o swagger de doctors

---
+++ app/Models/Swagger/DoctorSwagger.js
@@ -0,0 +1,21 @@
+/**
+* @swagger
+*  definitions:
+*    NewDoctor:
+*      type: object
+*      properties:
+*        name:
+*          type: string
+*        specialitie_id:
+*         type: integer
+*         format: "int32"
+*        user_id:
+*         type: integer
+*         format: "int32"
+*      required:
+*        - name
+*        - email
+*        - password
+*        - specialitie_id
+*        - user_id
+*/
