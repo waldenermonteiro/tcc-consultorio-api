5267e635f124c5021d231ee00fbbc32c6d52cb3c
Waldener Monteiro <jjrr019@gmail.com>
2020-05-07 05:48:25 -0300

    refactor: adicionando o repository no controller

--- app/Controllers/Http/MedicalScheduleController.js
+++ app/Controllers/Http/MedicalScheduleController.js
@@ -2,9 +2,14 @@
 const MedicalSchedule = use('App/Models/MedicalSchedule')
 const StoreMedicalSchedule = use('App/Validators/StoreMedicalSchedule')
 const BaseController = use('App/Controllers/Http/BaseController')
+const MedicalScheduleRepository = use('App/Repositories/MedicalScheduleRepository')
 class MedicalScheduleController extends BaseController {
   constructor() {
     super(MedicalSchedule, StoreMedicalSchedule)
+    this.MedicalScheduleRepository = new MedicalScheduleRepository(MedicalSchedule, StoreMedicalSchedule)
+  }
+  async index({ request, response, params }) {
+    return await this.MedicalScheduleRepository.index({ request, response, params })
   }
 }
 module.exports = MedicalScheduleController
