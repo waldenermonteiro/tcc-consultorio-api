351acbca4fdb16f0d9f82ab14a030cafb51228a9
Waldener Monteiro <jjrr019@gmail.com>
2020-05-07 05:49:52 -0300

    feature: criação do repsitory

---
+++ app/Repositories/MedicalScheduleRepository.js
@@ -0,0 +1,22 @@
+'use strict'
+const BaseRepository = use('App/Repositories/BaseRepository')
+class MedicalScheduleRepository extends BaseRepository {
+    constructor(Model, Validator) {
+        super(Model, Validator)
+        this.Model = Model
+        this.Validator = new Validator()
+    }
+    async index({ request, response, params }) {
+        try {
+            const items = await this.Model.query().filter(request.all()).fetch()
+            return response.ok({
+                status: 200,
+                data: items
+            })
+        } catch (error) {
+            return this.messageNotExistItem(response)
+        }
+    }
+
+}
+module.exports = MedicalScheduleRepository
