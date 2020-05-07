20bc8467564e107bf4c3d02af3d534313d076c3f
Waldener Monteiro <jjrr019@gmail.com>
2020-05-07 05:50:09 -0300

    hotfix: corrigindo as validações

--- app/Validators/StoreMedicalSchedule.js
+++ app/Validators/StoreMedicalSchedule.js
@@ -4,29 +4,33 @@ const existCustomRule = use('App/Validators/Customs/Exists')
 Validator.extend('exists', existCustomRule)
 class StoreDoctor {
   get name() {
-    return 'Agenda Médica'
+    return 'Consulta médica'
   }
   get inputs() {
-    return ['name', 'date_appointment', 'status', 'patient_id', 'prescription_medicament_id']
+    return ['name', 'date_appointment', 'status', 'patient_id', 'prescription_medicament_id', 'employee_id']
   }
-  rules(doctorId) {
-    doctorId = doctorId || 0
+  rules(medicalScheduleId) {
+    medicalScheduleId = medicalScheduleId || 0
     return {
-      name: `required`,
-      date_appointment: 'required',
+      name: `required|unique:medical_schedules,name,id,${medicalScheduleId}`,
+      date_appointment: `required`,
       status: 'required',
       patient_id: 'required|exists:patients,id',
-      prescription_medicament_id: 'exists:prescription_medicaments,id'
+      prescription_medicament_id: 'exists:prescription_medicaments,id',
+      employee_id: 'required|exists:employees,id'
     }
   }
   get messages() {
     return {
       'name.required': 'O campo nome é obrigatório.',
+      'name.unique': 'Já existe uma consulta médica com esse nome, por favor, escolha outro.',
       'date_appointment.required': 'O campo data da consulta é obrigatório.',
       'status.required': 'O campo status é obrigatório.',
       'patient_id.required': 'O campo paciente é obrigatório.',
       'patient_id.exists' :'Paciente não encontrado.',
-      'prescription_medicament_id.exists': 'Prescrição médica não encontrada.'
+      'prescription_medicament_id.exists': 'Prescrição médica não encontrada.',
+      'employee_id.required': 'O campo médico é obrigatório.',
+      'employee_id.exists': 'Médico não encontrado.',
     }
   }
 }
