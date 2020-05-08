130c0b17d30280dd333e561020d438696fd32436
Waldener Monteiro <jjrr019@gmail.com>
2020-05-08 06:10:00 -0300

    feature: criando e adicionando repository para request exam

--- app/Controllers/Http/RequestExamController.js
+++ app/Controllers/Http/RequestExamController.js
@@ -2,9 +2,14 @@
 const RequestExam = use('App/Models/RequestExam')
 const StoreRequestExam = use('App/Validators/StoreRequestExam')
 const BaseController = use('App/Controllers/Http/BaseController')
+const RequestExamRepository = use('App/Repositories/RequestExamRepository')
 class RequestExamController extends BaseController {
   constructor() {
     super(RequestExam, StoreRequestExam)
+    this.RequestExamRepository = new RequestExamRepository(RequestExam, StoreRequestExam)
+  }
+  async index({ request, response, params }) {
+    return await this.RequestExamRepository.index({ request, response, params })
   }
 }
 module.exports = RequestExamController
