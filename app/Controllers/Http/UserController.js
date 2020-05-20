9386cc44d3e2be9803936f727c1e119d3174dcc3
Waldener Monteiro <jjrr019@gmail.com>
2020-05-20 05:26:21 -0300

    refactor(users): passando employee e patient para o user repository

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,28 +1,30 @@
-'use strict'
-const User = use('App/Models/User')
-const StoreUser = use('App/Validators/StoreUser')
-const BaseController = use('App/Controllers/Http/BaseController')
-const UserRepository = use('App/Repositories/UserRepository')
+"use strict";
+const User = use("App/Models/User");
+const Patient = use("App/Models/Patient");
+const Employee = use("App/Models/Employee");
+const StoreUser = use("App/Validators/StoreUser");
+const BaseController = use("App/Controllers/Http/BaseController");
+const UserRepository = use("App/Repositories/UserRepository");
 
 class UserController extends BaseController {
-    constructor() {
-        super(User, StoreUser)
-        this.UserRepository = new UserRepository(User, StoreUser)
-    }
-    async login({auth, request, response, params }) {
-        return await this.UserRepository.login({ auth, request, response, params })
-    }
-    async auth({ auth, request, response, params }) {
-        const user = await User.find(params.id)
-        const token = await auth.generate(user, true)
-        return token
-    }
-    async index({ request, response }) {
-        return await this.UserRepository.index({ request, response })
-      }
-    async store({ auth, request, response }) {
-        return await this.UserRepository.store({ auth, request, response })
-    }
+  constructor() {
+    super(User, StoreUser);
+    this.UserRepository = new UserRepository(User, StoreUser, Patient, Employee);
+  }
+  async login({ auth, request, response, params }) {
+    return await this.UserRepository.login({ auth, request, response, params });
+  }
+  async auth({ auth, request, response, params }) {
+    const user = await User.find(params.id);
+    const token = await auth.generate(user, true);
+    return token;
+  }
+  async index({ request, response }) {
+    return await this.UserRepository.index({ request, response });
+  }
+  async store({ auth, request, response }) {
+    return await this.UserRepository.store({ auth, request, response });
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
