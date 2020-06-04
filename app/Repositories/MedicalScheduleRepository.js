e4235319ebbe95b9291ff852c2d490660ffd582b
Waldener Monteiro <jjrr019@gmail.com>
2020-06-04 04:28:04 -0300

    refactor(medical_schedules): criando o método de finalizar consulta, faltando validar mais precisamente

--- app/Repositories/MedicalScheduleRepository.js
+++ app/Repositories/MedicalScheduleRepository.js
@@ -1,11 +1,14 @@
 "use strict";
 const BaseRepository = use("App/Repositories/BaseRepository");
-const { validateAll } = use('Validator')
+const Database = use("Database");
+const { validateAll } = use("Validator");
 class MedicalScheduleRepository extends BaseRepository {
-  constructor(Model, Validator) {
+  constructor(Model, RequestExam, Validator, RequestExamValidator) {
     super(Model, Validator);
     this.Model = Model;
+    this.RequestExam = RequestExam;
     this.Validator = new Validator();
+    this.RequestExamValidator = new RequestExamValidator();
   }
   async index({ request, response, params }) {
     try {
@@ -33,6 +36,32 @@ class MedicalScheduleRepository extends BaseRepository {
       });
     } catch (error) {
       return this.messagesValidation(validation, response);
+    }
+  }
+  async finishConsult({ request, response, params }) {
+    const data = request.only(this.Validator.inputsFinishConsult);
+    const dataRequestExam = request.only(this.RequestExamValidator.inputsFinishConsult);
+    const validation = await validateAll(data.medicalSchedule, this.Validator.rulesFinishConsult(), this.Validator.messages);
+    const validationRequestExam = await validateAll(dataRequestExam.requestExam, this.RequestExamValidator.rules(), this.RequestExamValidator.messages);
+    const trx = await Database.beginTransaction();
+    try {
+      const medicalSchedule = await this.Model.findByOrFail("id", params.id);
+      await medicalSchedule.merge({ ...data.medicalSchedule, prescription_medicaments: JSON.stringify(data.medicalSchedule.prescription_medicaments), status: "Finalizada" }, trx);
+      await medicalSchedule.save();
+      if (dataRequestExam.requestExam) {
+        for (const rexam of dataRequestExam.requestExam) {
+          const requestExam = await this.RequestExam.create({ ...rexam, medical_schedule_id: params.id, status: "Agendado" }, trx);
+          requestExam.save();
+        }
+      }
+      await trx.commit();
+      return response.ok({
+        status: 200,
+        message: `Consulta finalizada com sucesso`,
+      });
+    } catch (error) {
+      await trx.rollback();
+      return dataRequestExam.requestExam ? this.messagesValidations([validationRequestExam, validation], response) : this.messagesValidations(validation, response);
     }
   }
 }
