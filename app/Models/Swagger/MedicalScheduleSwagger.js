bb92dc76c4cf9527281d6914bb2605a5a4ddc227
Waldener Monteiro <jjrr019@gmail.com>
2020-06-04 04:28:22 -0300

    refactor(medical_schedules): criando o swagger para o finalizar consulsta

--- app/Models/Swagger/MedicalScheduleSwagger.js
+++ app/Models/Swagger/MedicalScheduleSwagger.js
@@ -1,37 +1,67 @@
 /**
-* @swagger
-*  definitions:
-*    NewMedicalSchedule:
-*      type: object
-*      properties:
-*        name:
-*          type: string
-*        date_appointment:
-*          type: string
-*        observation:
-*          type: string
-*        status:
-*          type: string
-*        patient_id:
-*         type: integer
-*         format: "int32"
-*        prescription_medicament_id:
-*         type: integer
-*         format: "int32"
-*        employee_id:
-*         type: integer
-*         format: "int32"
-*      required:
-*        - name
-*        - date_appointment
-*        - status
-*        - patient_id
-*        - employee_id
-*    UpdateStatusMedicalSchedule:
-*      type: object
-*      properties:
-*        status:
-*          type: string
-*      required:
-*        - status
-*/
+ * @swagger
+ *  definitions:
+ *    NewMedicalSchedule:
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: string
+ *        date_appointment:
+ *          type: string
+ *        observation:
+ *          type: string
+ *        status:
+ *          type: string
+ *        patient_id:
+ *         type: integer
+ *         format: "int32"
+ *        prescription_medicament_id:
+ *         type: integer
+ *         format: "int32"
+ *        employee_id:
+ *         type: integer
+ *         format: "int32"
+ *      required:
+ *        - name
+ *        - date_appointment
+ *        - status
+ *        - patient_id
+ *        - employee_id
+ *    UpdateStatusMedicalSchedule:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *      required:
+ *        - status
+ *    FinishMedicalSchedule:
+ *      type: object
+ *      properties:
+ *        medicalSchedule:
+ *          type: object
+ *          properties:
+ *            observation:
+ *              type: string
+ *            prescription_medicaments:
+ *              type: integer
+ *        requestExam:
+ *          type: array
+ *          items:
+ *            type: object
+ *            properties:
+ *              observation:
+ *                type: string
+ *              type_exam_id:
+ *                type: integer
+ *                format: "int32"
+ *              medical_schedule_id:
+ *                type: integer
+ *                format: "int32"
+ *              status:
+ *                type: string
+ *            required:
+ *              - observation
+ *              - type_exam_id
+ *              - medical_schedule_id
+ *              - status
+ */
