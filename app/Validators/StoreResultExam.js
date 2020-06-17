afb97544e000a8ad74d9bfbaebf8ef2568f3a991
Waldener Monteiro <jjrr019@gmail.com>
2020-06-17 03:40:03 -0300

    refactor(result_exams): adicionando mais regras no validador

--- app/Validators/StoreResultExam.js
+++ app/Validators/StoreResultExam.js
@@ -4,15 +4,16 @@ const existCustomRule = use("App/Validators/Customs/Exists");
 Validator.extend("exists", existCustomRule);
 class StoreResultExam {
   get name() {
-    return "Solicitação de Exame";
+    return "Resultado de Exame";
   }
   get inputs() {
-    return ["result","request_exam_id"];
+    return ["result","request_exam_id", "medical_schedule_id"];
   }
   rules() {
     return {
       result: "required",
       request_exam_id: "required|exists:request_exams,id",
+      medical_schedule_id: "required|exists:medical_schedules,id"
     };
   }
   get messages() {
@@ -20,6 +21,8 @@ class StoreResultExam {
       "result.required": "O campo resultado é obrigatório.",
       "request_exam_id.required": " O campo requisição de exame é obrigatorio.",
       "request_exam_id.exists": "Requisição de exame não encontrado.",
+      "medical_schedule_id.required": " O campo consulta da requisição do de exame é obrigatorio.",
+      "medical_schedule_id.exists": "Consulta da requisição de exame não encontrada.",
     };
   }
 }
