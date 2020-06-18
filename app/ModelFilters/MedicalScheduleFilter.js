0611dd6ca33de8930297507d05899aae78d5d522
Waldener Monteiro <jjrr019@gmail.com>
2020-06-18 04:42:14 -0300

    refactor(medical_schedules): adicionando id como filtro

--- app/ModelFilters/MedicalScheduleFilter.js
+++ app/ModelFilters/MedicalScheduleFilter.js
@@ -3,6 +3,9 @@
 const ModelFilter = use("ModelFilter");
 
 class MedicalScheduleFilter extends ModelFilter {
+  id(id) {
+    return this.where("id", +id);
+  }
   name(name) {
     return this.where("name", "LIKE", `%${name}%`);
   }
