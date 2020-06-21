7fe16b5793e3bd1e78c67d4ae994ecbf858d0a31
Waldener Monteiro <jjrr019@gmail.com>
2020-06-21 02:17:52 -0300

    refactor(result_exams): odenando pela data e filtrando pela data tbm

--- app/ModelFilters/ResultExamFilter.js
+++ app/ModelFilters/ResultExamFilter.js
@@ -3,6 +3,12 @@
 const ModelFilter = use("ModelFilter");
 
 class ResultExamFilter extends ModelFilter {
+  createdAt(date) {
+    let newDateAddOneDay = new Date(date);
+    date = new Date(date);
+    newDateAddOneDay.setDate(newDateAddOneDay.getDate() + 1);
+    return this.whereBetween("created_at", [date, newDateAddOneDay]);
+  }
   medicalSchedule(id) {
     return this.where("medical_schedule_id", +id);
   }
