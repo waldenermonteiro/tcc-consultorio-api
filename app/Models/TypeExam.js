5005813b526db9550640846bb1e72b95ec8af0b8
Waldener Monteiro <jjrr019@gmail.com>
2020-06-22 16:46:47 -0300

    refactor(type_exams): atualizando os filtros do swagger e modificando o filtro.

--- app/Models/TypeExam.js
+++ app/Models/TypeExam.js
@@ -1,9 +1,17 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
+const Model = use("Model");
+const TypeExamFilter = use("App/ModelFilters/TypeExamFilter");
 class TypeExam extends Model {
+  static boot() {
+    super.boot();
+    // this.addTrait('@provider:Lucid/SoftDeletes')
+    this.addTrait("@provider:Filterable", TypeExamFilter);
+  }
+  // static get dates () {
+  //   return super.dates.concat(['deleted_at'])
+  // }
 }
 
-module.exports = TypeExam
+module.exports = TypeExam;
