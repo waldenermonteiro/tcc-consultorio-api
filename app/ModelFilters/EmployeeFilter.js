8001e3702f5adbed492192312e39d53d69571390
Waldener Monteiro <jjrr019@gmail.com>
2020-06-22 16:44:56 -0300

    refactor(employees): atualizando os filtros do swagger e modificando o filtro

--- app/ModelFilters/EmployeeFilter.js
+++ app/ModelFilters/EmployeeFilter.js
@@ -6,6 +6,9 @@ class EmployeeFilter extends ModelFilter {
   id(id) {
     return this.where("id", +id);
   }
+  name(name) {
+    return this.where("name", "LIKE", `%${name}%`);
+  }
   specialitie(id) {
     return this.where("specialitie_id", +id);
   }
