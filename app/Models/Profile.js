268c4eefcc5fa3eecd6d8a32837a1b6d6c040883
Waldener Monteiro <jjrr019@gmail.com>
2020-06-22 16:45:58 -0300

    refactor(profiles): atualizando os filtros do swagger e modificando o filtro

--- app/Models/Profile.js
+++ app/Models/Profile.js
@@ -2,12 +2,12 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
-
+const ProfileFilter = use('App/ModelFilters/ProfileFilter')
 class Profile extends Model {
     static boot() {
         super.boot()
-
         this.addTrait('@provider:Lucid/SoftDeletes')
+        this.addTrait('@provider:Filterable', ProfileFilter)
         
     }
     static get dates () {
