8c3811f41289b0115a7b0ae0fc08d16668fb2142
Waldener Monteiro <jjrr019@gmail.com>
2020-06-23 03:10:49 -0300

    refactor(type_exams): criando repository e realizando modificações para o destroy com validações

--- app/Controllers/Http/TypeExamController.js
+++ app/Controllers/Http/TypeExamController.js
@@ -2,9 +2,14 @@
 const TypeExam = use('App/Models/TypeExam')
 const StoreTypeExam = use('App/Validators/StoreTypeExam')
 const BaseController = use('App/Controllers/Http/BaseController')
+const TypeExamRepository = use("App/Repositories/TypeExamRepository");
 class TypeExamController extends BaseController {
   constructor() {
     super(TypeExam, StoreTypeExam)
+    this.TypeExamRepository = new TypeExamRepository(TypeExam, StoreTypeExam);
+  }
+  async destroy({ request, response, params }) {
+    return await this.TypeExamRepository.destroy({ request, response, params });
   }
 }
 module.exports = TypeExamController
