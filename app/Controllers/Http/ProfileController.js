e9698538fc71d93059b3b0638be36cbdeec9b446
Waldener Monteiro <jjrr019@gmail.com>
2020-06-23 03:10:23 -0300

    refactor(profiles): criando repository e realizando modificações para o destroy com validações

--- app/Controllers/Http/ProfileController.js
+++ app/Controllers/Http/ProfileController.js
@@ -2,9 +2,14 @@
 const Profile = use('App/Models/Profile')
 const StoreProfile = use('App/Validators/StoreProfile')
 const BaseController = use('App/Controllers/Http/BaseController')
+const ProfileRepository = use("App/Repositories/ProfileRepository");
 class ProfileController extends BaseController {
   constructor() {
     super(Profile, StoreProfile)
+    this.ProfileRepository = new ProfileRepository(Profile, StoreProfile);
+  }
+  async destroy({ request, response, params }) {
+    return await this.ProfileRepository.destroy({ request, response, params });
   }
 }
 module.exports = ProfileController
