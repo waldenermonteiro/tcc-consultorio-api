4a59b830876bf1ee85bf51f27cc835b30031227a
Waldener Monteiro <jjrr019@gmail.com>
2020-06-23 03:10:37 -0300

    refactor(specialities): criando repository e realizando modificações para o destroy com validações

--- app/Controllers/Http/SpecialitieController.js
+++ app/Controllers/Http/SpecialitieController.js
@@ -2,9 +2,14 @@
 const Specialitie = use('App/Models/Specialitie')
 const StoreSpecialitie = use('App/Validators/StoreSpecialitie')
 const BaseController = use('App/Controllers/Http/BaseController')
+const SpecialitieRepository = use("App/Repositories/SpecialitieRepository");
 class SpecialitieController extends BaseController {
   constructor() {
     super(Specialitie, StoreSpecialitie)
+    this.SpecialitieRepository = new SpecialitieRepository(Specialitie, StoreSpecialitie);
+  }
+  async destroy({ request, response, params }) {
+    return await this.SpecialitieRepository.destroy({ request, response, params });
   }
 }
 module.exports = SpecialitieController
