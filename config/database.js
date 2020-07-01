52cd172f4abb160a5a34516b291cffb4a8f63913
Waldener Monteiro <jjrr019@gmail.com>
2020-06-30 21:03:26 -0300

    refactor: retirando ssl

--- config/database.js
+++ config/database.js
@@ -75,8 +75,7 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
-      ssl: true
+      database: Env.get('DB_DATABASE', 'adonis')
     }
   }
 }
